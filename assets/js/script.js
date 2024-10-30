e509de6c08ee4b0764b79a4aa9dedbd765782a5b
Aleksa2024 <solevic.aleksa@yahoo.com>
2024-10-30 16:20:26 +0100

    Add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const startBtn = document.getElementById("start_btn");
+  const startWindow = document.getElementsByClassName("start_window");
+  const gameWindow = document.getElementsByClassName("game_window");
 
+  startBtn.addEventListener("click", initGame);
+
+  function initGame() {
+    console.log("Clicked", startBtn);
+  }
     
-
-
+    
 });
